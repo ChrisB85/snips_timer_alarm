14fe67682f92163c2ba7cbf999af7e3971fcf7b9
root <krzysztof.blachowicz@gmail.com>
2019-04-30 11:48:01 +0200

    snips_common

--- snips_common.py
+++ snips_common.py
@@ -1,6 +1,16 @@
 import mqtt_client
 import json
 
+
+def get_session_id(intent_message):
+    return intent_message.session_id
+
+def get_site_id(intent_message):
+    return intent_message.site_id
+
+def get_intent_name(intent_message):
+    return intent_message.intent.intent_name.split(':')[-1]
+
 def put_notification(site_id, text):
     data = {}
     data['siteId'] = site_id
